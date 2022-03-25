7e1ef2b611eb223b62637aa183d384a76f18ce5f
svmcd <spolat0750@gmail.com>
2022-03-25 13:00:16 +0100

    startfiles met animatie

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const element = document.getElementById("a1");
+
+element.addEventListener("click", function() {
+    var animation = bodymovin.loadAnimation({
+        container: document.getElementById('anim'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'lf30_u0hwvlj5.json'
+    });
+/*     window.open("https://www.google.com","_self")
+ */});
