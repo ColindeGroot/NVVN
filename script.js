d6dc4416132d4a70be25c22b487c01f999a429eb
svmcd <spolat0750@gmail.com>
2022-03-31 12:58:44 +0200

    eerste animatie is nu compleet

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 
-const b1 = document.getElementById("a1");
-
 var anim1  = bodymovin.loadAnimation({
     wrapper: document.getElementById('anim1'),
     renderer: 'svg',
     loop: false,
     autoplay: false,
-    path: 'https://assets1.lottiefiles.com/private_files/lf30_u8rbz7zb.json'
+    path: 'https://assets1.lottiefiles.com/private_files/lf30_gdpswqzd.json'
 });
 
-/* b1.addEventListener("click", () => {
-    anim1.play();
-}); */
+function addFlashyDesign() {
+    var element = document.getElementById("overlay");
+    element.classList.add("overlay-hide");
+}
 
-/* document.addEventListener('DOMContentLoaded', function() {
-    anim1.play();
-});
-
-window.onload = function() {
-    anim1.play();
-}; */
+window.onload = () => anim1.play(), setTimeout(addFlashyDesign, 3000);
