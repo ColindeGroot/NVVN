95378fc1c99250e30e0e67bac627267a94094b95
svmcd <spolat0750@gmail.com>
2022-04-01 12:52:06 +0200

    loading animatie toegevoegd

--- script.js
+++ script.js
@@ -5,11 +5,26 @@ var anim1  = bodymovin.loadAnimation({
     loop: false,
     autoplay: false,
     path: 'https://assets1.lottiefiles.com/private_files/lf30_gdpswqzd.json'
+}); 
+
+var loading  = bodymovin.loadAnimation({
+    wrapper: document.getElementById('loading'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets8.lottiefiles.com/packages/lf20_dkz94xcg.json'
 });
 
-function addFlashyDesign() {
-    var element = document.getElementById("overlay");
-    element.classList.add("overlay-hide");
+function hide() {
+    var overlay = document.getElementById("overlay");
+    var loading = document.getElementById("loading");
+    overlay.classList.add("overlay-hide");
+    loading.classList.add("loading-hide");
 }
 
-window.onload = () => anim1.play(), setTimeout(addFlashyDesign, 3000);
+function hideScroll() {
+    var element = document.querySelector('body');
+    element.classList.remove("water");
+}
+
+window.onload = () => anim1.play(), setTimeout(hide, 3000), setTimeout(hideScroll, 6000);
