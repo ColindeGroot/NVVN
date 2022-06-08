2b2a65d8e8e17a38f92d087e5aaf7624e6435d8c
Colin de Groot <colindegroot2004@gmail.com>
2022-06-08 14:15:07 +0200

    restructure +thempage

---
+++ colin/script.js
@@ -0,0 +1,7 @@
+var anim1  = bodymovin.loadAnimation({
+    wrapper: document.getElementById('anim1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/datafiles/dc49lw7cOTLEo6y/data.json'
+});
